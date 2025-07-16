7e367bee6fc36e90ebdb232d2b5581ea4f0a7a5a
John Sokpo <john.sokpo@hotmail.com>
2025-07-16 23:11:31 +0100

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ mongoose.connect(process.env.MONGO_URI, {
   .catch(err => console.log(err));
 
 // Sample route
-app.get('/', (req, res) => {
+app.get('/api/v1', (req, res) => {
     res.send('Backend is running');
 });
 
