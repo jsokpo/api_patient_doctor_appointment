0084431a927453403eb9642026dec67ef32abf36
John Sokpo <john.sokpo@hotmail.com>
2025-07-16 23:15:35 +0100

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ mongoose.connect(process.env.MONGO_URI, {
   .catch(err => console.log(err));
 
 // Sample route
-app.get('/', (req, res) => {
+app.get('https://healthsystemapi.onrender.com/api/v1', (req, res) => {
     res.send('Backend is running');
 });
 
